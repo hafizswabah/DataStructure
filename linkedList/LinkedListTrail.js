b439f48f6225c107a84e72efd8c6d124384d8677
hafizswabah <swabah.a.avd121@gmail.com>
2023-05-01 14:44:10 +0530

    deletion in linkedList

--- linkedList/LinkedListTrail.js
+++ linkedList/LinkedListTrail.js
@@ -31,30 +31,51 @@ class LinkedList {
         if (this.isEmpty()) {
             this.head = node
         } else {
-            let curr=this.head
-             while(curr.next){
-                 curr=curr.next 
-             }
-             curr.next=node
-       }
-       this.size++
+            let curr = this.head
+            while (curr.next) {
+                curr = curr.next
+            }
+            curr.next = node
+        }
+        this.size++
     }
-    insert(element,index){
-        if(index<0 || this.size<index ){
+    insert(element, index) {
+        if (index < 0 || this.size < index) {
             return
         }
-        if(index==0){
+        if (index == 0) {
             this.prepend(element)
-        }else{
-            const node=new Node(element)
-            let prev=this.head
-            for(let i=0;i<index-1;i++){
-                prev=prev.next
+        } else {
+            const node = new Node(element)
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
             }
-            node.next=prev.next
-            prev.next=node
+            node.next = prev.next
+            prev.next = node
             this.size++
         }
+    }
+    delete(index) {
+        if (index < 0 || index >= this.size) {
+            return 'Invalid Index'
+        }
+        let deletedNode
+        if (index === 0) {
+            deletedNode = this.head
+            this.head = this.head.next
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            deletedNode = prev.next
+            prev.next = deletedNode.next
+           
+        }
+        this.size--
+        return deletedNode.value
+
     }
     print() {
         if (this.isEmpty()) {
@@ -75,9 +96,8 @@ const linkedlist = new LinkedList()
 linkedlist.prepend(10)
 linkedlist.prepend(20)
 linkedlist.append(40)
-linkedlist.insert(25,0)
-linkedlist.insert(35,2)
+linkedlist.insert(25,3)
+linkedlist.delete(2)
 
 linkedlist.print()
 
-console.log(linkedlist.getSize());
