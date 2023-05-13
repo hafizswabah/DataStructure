85e1c33a662abd428290cf7036f5d7edae8551b0
hafizswabah <swabah.a.avd121@gmail.com>
2023-05-13 10:52:09 +0530

    binary Search Tree completed

--- BInarySearchTree/BinarySerachTree.js
+++ BInarySearchTree/BinarySerachTree.js
@@ -1,28 +1,31 @@
 class Node {
     constructor(value) {
         this.value = value
-        this.right = null
         this.left = null
+        this.right = null
     }
 }
+
 class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
     isEmpty() {
         return this.root === null
     }
+
     insert(value) {
         const node = new Node(value)
+
         if (this.isEmpty()) {
             this.root = node
-        }
-        else {
+        } else {
             this.insertNode(this.root, node)
         }
     }
-    insertNode(root, node) {
 
+    insertNode(root, node) {
         if (node.value < root.value) {
             if (root.left === null) {
                 root.left = node
@@ -30,36 +33,117 @@ class BinarySearchTree {
                 this.insertNode(root.left, node)
             }
         } else {
-            if (root.right === null) {
+            if (root.right == null) {
                 root.right = node
-            } else {
-                this.insertNode(root.right, node)
-            }
+            } else { this.insertNode(root.right, node) }
         }
-    
     }
-    searchNode(root, value) {
+
+    search(root, value) {
         if (!root) {
             return false
         } else {
-            if (root.value === value) {
-                return true
-            } else {
-                if (value < root.value) {
-                    return this.searchNode(root.left, value)
-                } else {
-                    return this.searchNode(root.right, value)
-                }
+            if (root.value == value) { return true }
+
+            else if (value < root.value) {
+                return this.search(root.left, value)
+            }
+            else {
+                return this.search(root.right, value)
             }
         }
     }
-}
-let bst = new BinarySearchTree()
-bst.insert(10)
 
+
+    preOrder(root) {
+        if (root) {
+            console.log(root.value);
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+
+    inOrder(root) {
+        if (root) {
+            this.inOrder(root.left)
+            console.log(root.value);
+            this.inOrder(root.right)
+        }
+    }
+    postOrder(root) {
+        if (root) {
+            this.postOrder(root.left)
+            this.postOrder(root.right)
+            console.log(root.value);
+        }
+    }
+    breadthFirstSearch() {
+        let queue = []
+        queue.push(this.root)
+        while (queue.length) {
+            let curr = queue.shift()
+            console.log(curr.value);
+            if (curr.left) {
+                queue.push(curr.left)
+            }
+            if (curr.right) {
+                queue.push(curr.right)
+            }
+        }
+    }
+    min(root) {
+        if (!root.left) {
+            return root.value
+        } else {
+            return this.min(root.left)
+        }
+    }
+    max(root) {
+        if (!root.right) {
+            return root.value
+        } else {
+            return this.max(root.right)
+        }
+    }
+    delete(value) {
+        this.root = this.deleteNode(this.root, value)
+    }
+    deleteNode(root, value) {
+        if (root === null) {
+            return root
+        }
+        if (value < root.value) {
+            root.left = this.deleteNode(root.left, value)
+        } else if (value > root.value) {
+            root.right = this.deleteNode(root.right, value)
+        } else {
+            if (!root.left && root.right) {
+                return null
+            }
+            if (!root.left) {
+                return root.right
+            }
+            else if (!root.right) {
+                return root.left
+            }
+            root.value = this.min(root.right)
+            root.right = this.deleteNode(root.right, root.value)
+        }
+        return root
+    }
+
+}
+
+let bst = new BinarySearchTree()
+
+
+bst.insert(10)
 bst.insert(5)
 bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+bst.delete(5)
+bst.breadthFirstSearch()
 
-console.log(bst.searchNode(15));
 
 
