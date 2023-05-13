ed1839e616ded1287d54d45caae0de4d64e7f383
hafizswabah <swabah.a.avd121@gmail.com>
2023-05-13 19:56:57 +0530

    Graph dataStructure Delete Vertex,Edge implemented

--- graph/graph.js
+++ graph/graph.js
@@ -1,42 +1,54 @@
-class Graph{
-    constructor(){
-        this.adjacencyList={}
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
     }
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]){
-            this.adjacencyList[vertex]=new Set()
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = new Set()
         }
     }
-    addEdge(vertex1,vertex2){
-        console.log(vertex1,'v1');
-        console.log(vertex2,'v2');
-        if(!this.adjacencyList[vertex1]){
+    addEdge(vertex1, vertex2) {
+        if (!this.adjacencyList[vertex1]) {
             this.addVertex(vertex1)
         }
-        if(!this.adjacencyList[vertex2]){
+        if (!this.adjacencyList[vertex2]) {
             this.addVertex(vertex2)
         }
         this.adjacencyList[vertex1].add(vertex2)
         this.adjacencyList[vertex2].add(vertex1)
     }
-    hasEdge(vertex1,vertex2){
+    hasEdge(vertex1, vertex2) {
         return (
             this.adjacencyList[vertex1].has(vertex2) &&
             this.adjacencyList[vertex2].has(vertex1)
 
         )
     }
-    display(){
-        for(let vertex in this.adjacencyList){
-            console.log(vertex + '=>' +[...this.adjacencyList[vertex]]);
+    display() {
+        for (let vertex in this.adjacencyList) {
+            console.log(vertex + '=>' + [...this.adjacencyList[vertex]]);
         }
     }
+    deleteEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].delete(vertex2)
+        this.adjacencyList[vertex2].delete(vertex1)
+    }
+    removeVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            return 'no vertex found'
+        }
+        for (let adjacentVertex of this.adjacencyList[vertex]) {
+            this.deleteEdge(vertex, adjacentVertex)
+        }
+
+        delete this.adjacencyList[vertex]
+    }
 }
-const graph=new Graph()
+const graph = new Graph()
 graph.addVertex('A')
 graph.addVertex('B')
 graph.addVertex('C')
-graph.addEdge('A','B')
-graph.addEdge('B','C')
+graph.addEdge('A', 'B')
+graph.addEdge('B', 'C')
+graph.removeVertex('A')
 graph.display()
-console.log(graph.hasEdge('A','C'));
